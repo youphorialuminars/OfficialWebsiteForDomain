6624a5ce1b1547198d5af5c1d7ba680688f46f0d
youphorialuminars <ananya21december@gmail.com>
2026-09-07 21:17:50 +0530

    feat: add luminarguide website source code - part 1

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+import { imageHosts } from './image-hosts.config.mjs';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  productionBrowserSourceMaps: true,
+  distDir: process.env.DIST_DIR || '.next',
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  images: {
+    remotePatterns: imageHosts,
+    minimumCacheTTL: 60,
+    qualities: [75, 85, 100],
+  }
+};
+export default nextConfig;
